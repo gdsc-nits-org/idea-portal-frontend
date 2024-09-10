065ad9c19a44de52209f16c6879c2f56d569466f
barnilsarma <barnilsarma@gmail.com>
2024-09-11 00:56:22 +0530

    some fixes

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${poppins.className}`}>
+      <div className={`${styles.main} ${poppins.className}`}>
         <Idea />
         <Form />
-      </main>
+      </div>
     </>
   );
 }
